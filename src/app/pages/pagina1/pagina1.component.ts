665c3b9b731a1e542c13097f88bb6f26ffe695f4
Jhonnatan Machuca <jfelipe.machuca@gmail.com>
2023-04-20 18:27:33 -0400

    Inicio LifeCycle
    Inicio LifeCycle

---
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,49 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styles: [
+  ]
+})
+export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterContentInit,
+                                         AfterContentChecked, AfterViewInit,
+                                         AfterViewChecked, OnDestroy {
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+}
